957e95a0f139a1af0e9f05affc85fede66b14117
Ahmed Naser Shebl <ahmednasershebl@yahoo.com>
2021-10-08 23:09:27 +0200

    Update OUT.PUT.py

--- OUT.PUT.py
+++ OUT.PUT.py
@@ -1,5 +1,3 @@
-Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:54:40) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
 >>> Hello! Let's explore some US bikeshare data!
 which city ? chicago , new york city , washington ? washington
  which month ? from january to june >>April
